04d30ba49956f6b53072093fa7e46942d0b0a3c2
akashdeep-singh-multani <Akashdeepm@mindfiresolutions.com>
2024-10-10 22:48:05 +0530

    Delete all previous code on master branch

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserPostComponent } from './components/user-post/user-post.component';
-
-export const routes: Routes = [
-    {
-        path: '**',
-        component: UserPostComponent
-    }
-];
