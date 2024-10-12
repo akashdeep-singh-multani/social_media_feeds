c5a304092366e3ca006d1d4e7834e738f3a45040
akashdeep-singh-multani <Akashdeepm@mindfiresolutions.com>
2024-10-12 14:08:14 +0530

    added create-post component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { UserPostComponent } from './components/user-post/user-post.component';
 
 export const routes: Routes = [
     {
-        path: '**',
+        path: '',
         component: UserPostComponent
+    },
+    {
+        path:'create_post',
+        loadComponent: ()=>import('./components/create-post/create-post.component').then(m=>m.CreatePostComponent) 
     }
 ];
--- src/app/components/user-post/user-post.component.ts
+++ src/app/components/user-post/user-post.component.ts
@@ -5,15 +5,22 @@ import {MatIconModule} from '@angular/material/icon';
 import { LikeButtonComponent } from '../like-button/like-button.component';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { CommentButtonComponent } from '../comment-button/comment-button.component';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-post',
   standalone: true,
-  imports: [UserProfileComponent, CommentButtonComponent,LikeButtonComponent,NgClass,MatCardModule, MatIconModule],
+  imports: [MatButtonModule,UserProfileComponent, CommentButtonComponent,LikeButtonComponent,NgClass,MatCardModule, MatIconModule],
   templateUrl: './user-post.component.html',
   styleUrl: './user-post.component.css'
 })
 export class UserPostComponent {
 
+  constructor(private router: Router){}
+
+  handleCreatePostClick(){
+    this.router.navigate(['create_post']);
+  }
   
 }
