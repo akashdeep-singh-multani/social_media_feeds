d4936f5024015481ae0b816c69700f4a6c763863
akashdeep-singh-multani <akashdeepm@mindfiresolutions.com>
2024-10-13 03:27:47 +0530

    split frontend and backend

---
+++ backend/models/post.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+
+const postSchema=new mongoose.Schema({
+    title: String,
+    content: String,
+    image: String,
+    created_at: {type: Date, Default: Date.now}
+});
+
+module.exports=mongoose.model('Post', postSchema);
